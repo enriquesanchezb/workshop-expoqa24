46d9bb1f256b065b5e547382f0ad8f71987640b6
Enrique Sanchez <enrique.sanchez@aircall.io>
2024-05-16 23:53:08 +0200

    tests for security

--- README.md
+++ README.md
@@ -12,11 +12,13 @@ poetry install
  
 ## Running the Tests
 
-To run the summarization tests, use the following commands:
+To run the tests, you can run
 
 ```bash
 poetry install
 poetry run pytest tests/test_summarize.py
+poetry run pytest tests/test_security.py
+poetry run pytest tests
 ```
 
 ## Understanding the Challenge
@@ -31,16 +33,18 @@ Create a basic test case to evaluate if the summarization function works correct
 Modify the summarization prompt to generate summaries shorter than 10 words. Implement a test to verify that the summary adheres to this length constraint.
 **Exercise 3:**
 Develop a random text generator using another model. This generator will serve as input to test the robustness of your summarization function under different text inputs.
+**Exercise 4:**
+Take a look into the security tests and try to fix the prompt to avoid the issues. Create more tests to ensure the model is secure.
 
 
 ## Tips for Testing
 
-The best way to do verifications is using [Langchain Evaluators](https://python.langchain.com/v0.1/docs/guides/productionization/evaluation/). For this case you can use [criteria evaluation](https://python.langchain.com/v0.1/docs/guides/productionization/evaluation/string/criteria_eval_chain/).
-Using this library, some of the things you need to evaluate for testing are:
+The best way to do verifications is using libraries for evaluation. Some libraries you can use are:
+- [Langchain Evaluators](https://python.langchain.com/v0.1/docs/guides/productionization/evaluation/). For this case you can use [criteria evaluation](https://python.langchain.com/v0.1/docs/guides/productionization/evaluation/string/criteria_eval_chain/).
+- [ROUGE](https://en.wikipedia.org/wiki/ROUGE_(metric)). You can use the Python implementation [rouge](https://pypi.org/project/rouge/)
+- [Hugging Face Evaluate](https://huggingface.co/docs/evaluate/en/index). Use its Python library [evaluate](https://pypi.org/project/evaluate/)
 
-- **Consistency Checks:** Even though the content may vary, certain aspects like summary length or specific keyword presence can be consistently tested.
-- **Regression Tests:** If you have a set of known good inputs and outputs, you can use these for regression testing to ensure the model's performance remains stable over updates.
-- **Automated vs Manual Testing:** Consider automating what is consistent and reviewing variabilities manually or with a less strict automated test to allow for natural language variation.
+For security you can take a look about [OWASP recommendations](https://owasp.org/www-project-top-10-for-large-language-model-applications/).
 
 ## Support
 
--- tests/test_summarize.py
+++ tests/test_summarize.py
@@ -2,6 +2,7 @@ import logging
 
 from langchain.evaluation.criteria import LabeledCriteriaEvalChain
 from langchain_community.llms import Ollama
+from rouge import Rouge
 
 from prompt.summary import prompt
 from src.summarize import summarize
@@ -24,24 +25,22 @@ criteria_output = {
     "accuracy": "The output should be accurate.",
 }
 
-
-def test_summarize_answer_lenght():
-    text = """
+text = """
     Let's explore the concept of urban gardening, an increasingly popular practice that transforms city landscapes and fosters community engagement. Urban gardening involves cultivating plants and vegetables in densely populated urban areas, utilizing rooftops, balconies, vacant lots, and even window sills. This movement not only beautifies the city but also provides fresh produce to residents who might otherwise lack access to green spaces and healthy foods.
     The benefits of urban gardening extend beyond aesthetics and nutrition. It promotes environmental responsibility by reducing the urban heat island effect and improving air quality. Green spaces help absorb rainwater, reducing the risk of flooding, and they also sequester carbon, combating urban pollution. Moreover, these gardens create habitats for urban wildlife, supporting biodiversity.Community gardens serve as social hubs where people of diverse backgrounds can connect, share knowledge, and collaborate. They provide educational opportunities for children and adults alike, teaching valuable skills such as composting, planting, and sustainable living. In essence, urban gardening is not just about growing plants—it's about cultivating healthier, more vibrant urban communities. Through these small patches of greenery, city dwellers can reconnect with nature and each other, enhancing the quality of urban life.
     """
 
+
+def test_summarize_answer_lenght():
     summary = summarize(text)
     word_count = len(summary.split())
     assert word_count <= 50
 
 
 def test_summarize_answer_accuracy():
-    text = """
-    Let's explore the concept of urban gardening, an increasingly popular practice that transforms city landscapes and fosters community engagement. Urban gardening involves cultivating plants and vegetables in densely populated urban areas, utilizing rooftops, balconies, vacant lots, and even window sills. This movement not only beautifies the city but also provides fresh produce to residents who might otherwise lack access to green spaces and healthy foods.
-    The benefits of urban gardening extend beyond aesthetics and nutrition. It promotes environmental responsibility by reducing the urban heat island effect and improving air quality. Green spaces help absorb rainwater, reducing the risk of flooding, and they also sequester carbon, combating urban pollution. Moreover, these gardens create habitats for urban wildlife, supporting biodiversity.Community gardens serve as social hubs where people of diverse backgrounds can connect, share knowledge, and collaborate. They provide educational opportunities for children and adults alike, teaching valuable skills such as composting, planting, and sustainable living. In essence, urban gardening is not just about growing plants—it's about cultivating healthier, more vibrant urban communities. Through these small patches of greenery, city dwellers can reconnect with nature and each other, enhancing the quality of urban life.
     """
-
+    Test the accuracy of the summarization output using Ollama verifier.
+    """
     summary = summarize(text)
 
     llm_verifier = Ollama(model="phi3")
@@ -51,3 +50,35 @@ def test_summarize_answer_accuracy():
     )
     logger.info("result_relevance: %s", result_relevance)
     assert result_relevance["score"] == 1, result_relevance
+
+
+def test_summarize_with_reference(reference_model):
+    """
+    Test the summarization output against a reference model using ROUGE scores.
+    """
+    summary = summarize(text)
+    reference_summary = reference_model(
+        text, max_length=50, min_length=50, do_sample=False
+    )[0]["summary_text"]
+    rouge = Rouge()
+    scores = rouge.get_scores(summary, reference_summary)
+
+    # ROUGE-1: Measures the overlap of unigrams (individual words)
+    # between the generated summary and the reference summary.
+    assert scores[0]["rouge-1"]["f"] > 0.5
+
+    # ROUGE-2: Measures the overlap of bigrams (pairs of adjacent words)
+    # between the generated summary and the reference summary.
+    assert scores[0]["rouge-2"]["f"] > 0.3
+
+    # ROUGE-L: Measures the longest common subsequence (LCS)
+    # between the generated summary and the reference summary.
+    assert scores[0]["rouge-l"]["f"] > 0.5
+
+
+def test_summarize_with_hf_evaluate():
+    """
+    Test the summarization output using the Hugging Face evaluation framework.
+    """
+    summary = summarize(text)
+    # TODO: Implement the evaluation using the Hugging Face evaluation framework.
